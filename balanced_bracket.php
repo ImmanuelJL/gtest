8c511740d57d3859412eebc86f24d2ef01cb0667
it-dba04 <it-dba04@lippoinsurance.com>
2023-06-13 15:17:27 +0700

    done

--- balanced_bracket.php
+++ balanced_bracket.php
@@ -1,3 +1,26 @@
 <?php
 
 $str = '{[()]}';
+// $str = '{[(])}';
+// $str = '{(([])[])[]}';
+// $str = '[()]{}{[()()]()}';
+// $str = '[(])';
+
+$arr = str_split($str);
+
+$q = ['{', '(','['];
+$w = ['}', ')',']'];
+
+foreach ($arr as $keyChar => $char) {
+	if (in_array($char, $q)) {
+    	$keyQ = array_search($char, $q);
+
+		if (in_array($arr[$keyChar+1], $w) && $arr[$keyChar+1] !== $w[$keyQ]) {
+			echo 'NO';
+
+    		return false;
+		}
+    }
+}
+
+echo 'YES';
